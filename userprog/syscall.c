ba624df13d453a81986344e10197720aa891442c
kevinwu52 <k.wu522@gmail.com>
2016-03-23 18:55:27 -0400

    Updated syscall.h and syscall.c. Switch case in handler not yet implemented fully.

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -8,6 +8,7 @@
 #include "filesys/filesys.h"
 #include "filesys/file.h"
 #include "lib/kernel/list.h"
+#include "threads/synch.h"
 
 #include "devices/input.h"
 /* End of header files added in */
@@ -15,12 +16,27 @@
 /* These Global variables were added in */
 #define MAX_FILE_SIZE 204800
 #define MAX_FILE_NAME_LENGTH 64
+#define MAX_ARGS 5
 
-struct file* process_get_file (int fd);
+struct file* file_check(int fd);
 static struct lock LOCK;
+
+struct process_file
+{
+	struct file *file;
+	int fd;
+	struct list_elem elem;
+};
 /* End of Global variables added in */
 
 static void syscall_handler (struct intr_frame *);
+
+/* The following were added in */
+int process_file_add (struct file *f);
+struct file* process_file_get( int fd);
+void arg_get (struct intr_frame *f, int *arg, int n);
+
+/* End of elements aded in     */
 
 /* Functions */
 static int read(int fd, void *buffer, unsigned size);
@@ -29,6 +45,8 @@ static bool create (const char *file, unsigned initial_size);
 static bool remove (const char *file);
 static int open (const char *file); 
 static int filesize (int fd);
+static void seek(int fd, unsigned position);
+static unsigned tell(int fd);
 
 void
 syscall_init (void) 
@@ -39,8 +57,102 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+	/*int arg[MAX_ARGS];
+	switch (* (int *) f-> esp);
+	{
+		case SYS_HALT:
+		{
+			halt();
+			break;
+		}
+
+		case SYS_EXIT:
+		{
+			arg_get(f, &arg[0], 1);
+			exit(arg[0]);
+			break;
+		}
+
+		case SYS_EXEC:
+		{
+			arg_get(f, &arg[0], 1);
+			//If the following line is needed, user_to_kernel_ptr needs to be implemented
+			//arg[0] = user_to_kernel_ptr((const void *) arg[0]);
+			f -> eax = exec((const char *) arg[0]);
+			break;
+		}
+
+		case SYS_WAIT:
+		{
+			arg_get(f, &arg[0], 1);
+			f -> eax = wait(arg[0]);
+			break;
+		}
+
+		case SYS_CREATE:
+		{
+			arg_get(f, &arg[0], 1);
+			f -> eax = create((const char *) arg[0], (unsigned) arg[1]);
+			break;
+		}
+
+		case SYS_REMOVE:
+		{
+			arg_get(f, &arg[0], 1);
+			f -> eax = remove((const char *) arg[0]);
+			break;
+		}
+
+		case SYS_OPEN:
+		{
+			arg_get(f, &arg[0], 1);
+			f -> eax = open((const char *) arg[0]);
+			break;
+		}
+
+		case SYS_FILESIZE:
+		{
+			arg_get(f, &arg[0], 1);
+			f -> eax = filesize(arg[0]);
+			break;
+		}
+
+		case SYS_READ:
+		{
+			arg_get(f, &arg[0], 1);
+			//INSPIRED LINE...
+			//arg[1] = user_to_kernel_ptr((const void *) arg[1]);
+			//f -> eax = read(arg[0], (void *) arg[1], (unsigned) arg[2]);
+			break;
+		}
+
+		case SYS_WRITE:
+		{
+			arg_get(f, &arg[0], 1);
+			seek(arg[0], (unsigned) arg[1]);
+			break;
+		}
+
+		case SYS_TELL:
+		{
+			arg_get(f, &arg[0], 1);
+			f -> eax = tell(arg[0]);
+			break;
+		}
+
+		case SYS_CLOSE:
+		{
+			arg_get(f, &arg[0], 1);
+			close(arg[0]);
+			break;
+		}
+
+	}*/
+
+  //This was originally here
   printf ("system call!\n");
   thread_exit ();
+  
 }
 
 //TO DO: syscalls: halt, exit, exec, wait, remove, open, filesize, read, write, seek, tell, close
@@ -49,6 +161,8 @@ syscall_handler (struct intr_frame *f UNUSED)
 static bool
 create (const char *file, unsigned initial_size)
 {	
+	lock_acquire(&LOCK);
+
 	//Case 1: "initial_size" is lower than the limit
 	if (initial_size <= MAX_FILE_SIZE){
 		//Must check if file name length is lower than limit too
@@ -61,8 +175,9 @@ create (const char *file, unsigned initial_size)
 		//file name length and initial size are lower than the limits...
 		//...then it's ok to create the file then
 		else{
-			return filesys_create(file, initial_size);
-			//NOTE: filesys_create is a built in function located in "filesys.c"
+			bool file_created = filesys_create(file, initial_size);
+			lock_release(&LOCK);
+			return file_created;
 		}
 	}
 	//Case 2: "initial_size" is larger than the limit
@@ -70,117 +185,58 @@ create (const char *file, unsigned initial_size)
 		printf("File size is too large!");
 	}
 
+lock_release(&LOCK);
 return false;
 }
 
 static bool
 remove (const char *file)
-{
-	return filesys_remove(file);
+{	
+	lock_acquire(&LOCK);
+	bool file_removed = filesys_remove(file);
+	lock_release(&LOCK);
+	return file_removed;
 }
 
 static int 
 open (const char *file) 
 {
-
+	lock_acquire(&LOCK);
 	if (strlen(file) == 0) {
-//		printf("File open failed because file name can not be empty!\n");
-		;
-	} else {
-
-		//if the file is required to be removed,
-		//it is invisible for opening operation.
-
-		struct list_elem *rfe = list_begin(&removing_list);
-
-//		printf("rfe == NULL ? %d\n", rfe==NULL);
-//		printf("removing list size = %d\n", list_size(&removing_list));
-
-		while(rfe != list_end(&removing_list)) {
-			struct list_elem *tmprfe = rfe;
-			rfe = list_next(rfe);
-			struct removing_file *rf = list_entry(tmprfe, struct removing_file, r_elem);
-			if (strcmp(rf->file_name, file) == 0) {
-				return -1;
-			}
-		}
-
-		//if the file is not required to be removed,
-		//it can be opened.
-		//lock_acquire(&fd_lock);
-		struct file *f = filesys_open(file);
-		if (f != NULL) {
-			uint32_t n_fd = global_fd++;
-
-			//fd == 1 screen
-			//fd == 0 keyboard
-			//fd >= 2 file
-			if (n_fd >= 2) {
-				struct list *fd_list = thread_add_fd(thread_current(), n_fd, f, file);
-//				printf("fd_list size = %d\n", list_size(fd_list));
-				if (fd_list != NULL) {
-					if (list_size(fd_list) == 1) {
-						struct fds_stub *fdss = malloc(sizeof(struct fds_stub));
-						fdss->l = fd_list;
-						list_push_back(&fds_ll, &fdss->fds_elem);
-//						printf("fds_ll_size = %d\n", list_size(&fds_ll));
-					}
-				}
-			}
-
-			//lock_release(&fd_lock);
-			return n_fd;
-		}
-		//lock_release(&fd_lock);
-	}
-
+		printf("File open failed because file name can not be empty!\n");
 	return -1;
+	} 
+	else 
+	{
+		struct file *f = filesys_open(file);
+		if (!f)
+		{
+			lock_release(&LOCK);
+		}
+		int fd = process_file_add(f);
+		lock_release(&LOCK);
+		return fd;
+	}
 }
 
 static int
 filesize (int fd)
-{
-	struct file *f = get_file_p(fd);
+{	
+	lock_acquire(&LOCK);
+	
+	struct file *f = process_file_get(fd);
 	if (f != NULL){
+		lock_release(&LOCK);
 		return file_length(f);
+	}
+	else
+	{
+		lock_release(&LOCK);
+		return -1;
 	}
 }
 
 /* The following function was added in */
-
-//This function is used to get the pointer of the file structure
-struct file* get_file_p(unsigned int fd) {
-
-	if (fd == 1 || fd == 0) {
-		//Cannot get the file structure pointer if fd is either 0 or 1...
-		return NULL;
-	}
-
-	struct list_elem *le = list_begin(&fds_ll);
-	while(le != list_end(&fds_ll)) {
-		struct list_elem *tmp = le;
-		le = list_next(le);
-		struct fds_stub *fdss = list_entry(tmp, struct fds_stub, fds_elem);
-		if (fdss != NULL) {
-			struct list * fd_list = fdss->l;
-			if (fd_list != NULL) {
-				struct list_elem *fd_le = list_begin(fd_list);
-				while(fd_le != list_end(fd_list)) {
-					struct list_elem *tmp_fd_le = fd_le;
-					fd_le = list_next(fd_le);
-					struct fd_elem *fde = list_entry(tmp_fd_le, struct fd_elem, fdl_elem);
-					if (fde != NULL) {
-						if (fde->fd == fd) {
-
-							return fde->f;
-						}
-					}
-				}
-			}
-		}
-	}
-	return NULL;
-}
 
 /* Reads size bytes from buffer to open file fd. Returns the 
 number of bytes actually read (0 at end of file), or -1 if the file
@@ -193,7 +249,7 @@ read(int fd, void *buffer, unsigned size)
 	//declared 'offset' outside of for loop because of error in compliation
 	unsigned offset;
 
-	//lock_acquire(&LOCK);
+	lock_acquire(&LOCK);
 	
 	//read syscall
 	if (fd == STDIN_FILENO){
@@ -208,26 +264,16 @@ read(int fd, void *buffer, unsigned size)
 		returnVal = -1;
 	}
 	
-	//lock_release(&LOCK);
+	lock_release(&LOCK);
 	return returnVal;
 }
 
-/* Writes size bytes from buffer to the open file fd. Returns the number of bytes actually
-written, which may be less than size if some bytes could not be written.
-Writing past end-of-file would normally extend the file, but file growth is not implemented
-by the basic file system. The expected behavior is to write as many bytes as
-possible up to end-of-file and return the actual number written, or 0 if no bytes could
-be written at all.
-Fd 1 writes to the console. Your code to write to the console should write all of buffer
-in one call to putbuf(), at least as long as size is not bigger than a few hundred
-bytes. (It is reasonable to break up larger buffers.) Otherwise, lines of text output
-by different processes may end up interleaved on the console, confusing both human
-readers and our grading scripts. */
+
 static int 
 write(int fd, const void *buffer, unsigned size)
 {
 	int returnVal;
-	//lock_acquire(&LOCK);
+	lock_acquire(&LOCK);
 
 	//write sycall
 	if (fd == STDOUT_FILENO) {
@@ -236,19 +282,84 @@ write(int fd, const void *buffer, unsigned size)
 		goto done;
 	}
 	
-	struct file *f = process_get_file(fd);
+	struct file *f = file_check(fd);
 	if (!f){
-		//lock_release(&LOCK);
+		lock_release(&LOCK);
 		returnVal = -1;
 	} else {
 		returnVal = file_write(f, buffer, size);
 	}
-	//lock_release(&LOCK);
+	lock_release(&LOCK);
 	
 	done:
 	  return returnVal;
 }
 
+static void
+seek(int fd, unsigned position)
+{
+	lock_acquire(&LOCK);
+
+    struct file *f = file_check(fd);
+
+    if (!f) {
+     goto done;
+    }
+    file_seek(f, position);
+
+	done:
+	  lock_release(&LOCK);
+	  return;
+}
+
+static unsigned
+tell(int fd)
+{
+	lock_acquire(&LOCK);
+	struct file *f = file_check(fd);
+  	if (!f){
+      	lock_release(&LOCK);
+      	return -1;
+    }
+  	lock_release(&LOCK);
+  	return file_tell(f);
+}
+
+void arg_get (struct intr_frame *f, int *arg, int n)
+{
+	int *ptr;
+	for (int i = 0; i < n; i++)
+	{
+		ptr = (int *) f->esp + i + 1;
+		arg[i] = *ptr;
+	}
+}
+
+int process_file_add (struct file *f)
+{
+	struct process_file *pf = malloc(sizeof(struct process_file));
+	pf -> file = f;
+	pf -> fd = thread_current() -> fd;
+	thread_current() -> fd++;
+	list_push_back(&thread_current() -> file_list, &pf -> elem);
+	return pf -> fd;
+}
+
+struct file* process_file_get (int fd)
+{
+	struct thread *t = thread_current();
+	struct list_elem *e;
+
+	for(e = list_begin( &t -> file_list); e != list_end(&t -> file_list); e = list_next(e))
+	{
+		struct process_file *pf = list_entry(e, struct process_file, elem);
+		if( fd == pf -> fd)
+		{
+			return pf -> file;
+		}
+	}
+return NULL;
+
+}
 
 /* End of function added in */
-
