ac0e6d87b3d849c9495e4e774757ca46da196073
kevinwu52 <k.wu522@gmail.com>
2016-03-24 20:35:41 -0400

    updated syscall.h

--- userprog/syscall.h
+++ userprog/syscall.h
@@ -3,11 +3,8 @@
 
 void syscall_init (void);
 
-<<<<<<< HEAD
 /* The following were added in */
 
 /* End of elements added in */
 
-=======
->>>>>>> 4d084357565aadfe2876e6b37ffeb43698ddd9cb
 #endif /* userprog/syscall.h */
