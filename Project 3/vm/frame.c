646b2f462d31ccb3515081ec370be67853b37a5c
Vineet Sepaha <Vineet Sepaha>
2016-04-19 11:42:23 -0400

    frame files updated

---
+++ Project 3/vm/frame.c
@@ -0,0 +1,142 @@
+#include "vm/frame.h"
+#include "threads/malloc.h"
+#include "threads/palloc.h"
+#include "threads/thread.h"
+#include "userprog/pagedir.h"
+#include "userprog/syscall.h"
+#include "vm/page.h"
+#include "vm/swap.h"
+#include "filesys/file.h"
+#include "threads/synch.h"
+
+
+
+// Initialization of the frame table
+void frame_tbl_init(void) {
+	lock_init(&frame_tbl_lock); // Lock for the frame table
+	list_init(&frame_tbl_list); // List to keep track of the frames in the table
+}
+
+// function used to add a frame to the table
+void add_frame_to_tbl(void *frame, struct suppl_page_table_ent *spte) {
+  struct frame_tbl_ent *fte = malloc(sizeof(struct frame_tbl_ent)); // allocate memory the size of the frame table
+
+  // set the parameters for the frmane table entry
+  fte->frame = frame;
+  fte->spte = spte;
+  fte->thread = thread_current();
+
+  // add frame table entry on the frame table list
+  lock_acquire(&frame_tbl_lock);
+  list_push_back(&frame_tbl_list, &fte->elem);
+  lock_release(&frame_tbl_lock);
+}
+
+// allocate memory for the frame table
+void* frame_alloc(enum palloc_flags flags, struct suppl_page_table_ent *spte) {
+
+// safeguard to ensure that PAL_USER is called 
+  if ( (flags & PAL_USER) == 0 ) {
+      return NULL;
+    }
+
+// Since we ensured that PAL_USER is set, the page is obtained 
+// from the user pool
+  void *frame = palloc_get_page(flags);
+
+// If the page is successfully obtained from the user pool
+	if (frame) {
+  		// add the frame to the table
+    	add_frame_to_tbl(frame, spte);
+    } else {
+    // when no frame is free, a frame must be made free through eviction algo
+     	while (!frame) {
+			frame = frame_evict(flags);
+		  	lock_release(&frame_tbl_lock);
+		}
+
+	// no frame can be evicted without allocating a swap slot,
+	// but swap slot is full -> panic the kernel
+     	if (!frame) {
+	  		PANIC("Frame could not be evicted");
+		}
+
+	//add the frame to table which has been freed up
+      	frame_add_to_table(frame, spte);
+    }
+
+    // return the frame
+	return frame;
+}
+
+
+void free_frame(void *frame) {
+
+	//acquire lock
+	lock_acquire(&frame_tbl_lock);
+
+	struct list_elem *e;
+  	for (e = list_begin(&frame_tbl_list); e != list_end(&frame_tbl_list);
+       e = list_next(e)) {
+
+      struct frame_tbl_ent *fte = list_entry(e, struct frame_tbl_ent, elem);
+
+      if (fte->frame == frame) {
+		  list_remove(e);
+		  free(fte);
+		  palloc_free_page(frame);
+		  break;
+		}
+    }
+    //release the lock
+	lock_release(&frame_tbl_lock);
+}
+
+
+void* frame_evict (enum palloc_flags flags) {
+  
+	lock_acquire(&frame_tbl_lock);
+
+	struct list_elem *e = list_begin(&frame_tbl_list);
+
+	while (true) {
+    	struct frame_tbl_ent *fte = list_entry(e, struct frame_tbl_ent, elem);
+
+      	if (!fte->spte->pinned) {
+	  		struct thread *t = fte->thread;
+
+	  		if (pagedir_is_accessed(t->pagedir, fte->spte->uva)) {
+	      		pagedir_set_accessed(t->pagedir, fte->spte->uva, false);
+	    	} else {
+	      		if (pagedir_is_dirty(t->pagedir, fte->spte->uva) ||
+					fte->spte->type == SWAP) {
+
+		  			if (fte->spte->type == MMAP) {
+		      			lock_acquire(&filesys_lock);
+		      			file_write_at(fte->spte->file, fte->frame,
+				    	fte->spte->read_bytes,
+				   		fte->spte->offset);
+		      			lock_release(&filesys_lock);
+		    		} else {
+		      			fte->spte->type = SWAP;
+		      			fte->spte->swap_index = swap_out(fte->frame);
+		    		}
+
+				}
+
+		      fte->spte->is_loaded = false;
+		      list_remove(&fte->elem);
+		      pagedir_clear_page(t->pagedir, fte->spte->uva);
+		      palloc_free_page(fte->frame);
+		      free(fte);
+		      return palloc_get_page(flags);
+		    }
+
+		}
+     	e = list_next(e);
+    	if (e == list_end(&frame_tbl_list)) {
+      		e = list_begin(&frame_tbl_list);
+		}
+    }
+}
+
---
+++ Project 3/vm/frame.h
@@ -0,0 +1,35 @@
+#ifndef VM_FRAME_H
+#define VM_FRAME_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <list.h>
+#include "vm/page.h"
+#include "threads/palloc.h"
+#include "threads/synch.h"
+
+
+struct list frame_tbl_list;
+struct lock frame_tbl_lock;
+
+// struct for the frame table entry
+struct frame_tbl_ent {
+  	void *frame; // frame in the entry
+  	struct thread *thread; //thread for the frame table entry
+  	struct suppl_page_table_ent *spte; // supplemental page table enrty
+  	struct list_elem elem; // list element
+};
+
+
+// functions defined in frame.c //
+// Used to initialize the frame table
+void frame_tbl_init(void); 
+// Used to add a framme to the table
+void add_frame_to_tbl (void *frame, struct sup_page_entry *spte);
+// allocate memory for the frame
+void* frame_alloc(enum palloc_flags flags, struct sup_page_entry *spte);
+// 
+void free_frame(void *frame);
+void* frame_evict(enum palloc_flags flags);
+
+#endif /* vm/frame.h */
