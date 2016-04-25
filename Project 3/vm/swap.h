b51116ec4d2e4bb7a1b1ebb60913ac9225d305f2
Vineet Sepaha <Vineet Sepaha>
2016-04-25 19:14:04 -0400

    new swap files

--- Project 3/vm/swap.h
+++ Project 3/vm/swap.h
@@ -1,22 +1,20 @@
 #ifndef VM_SWAP_H
 #define VM_SWAP_H
 
+#include <bitmap.h>
 #include "devices/block.h"
 #include "threads/synch.h"
 #include "threads/vaddr.h"
-#include <bitmap.h>
 
+#define SECTORS_PER_PAGE (PGSIZE / BLOCK_SECTOR_SIZE)
 #define SWAP_FREE 0
 #define SWAP_IN_USE 1
 
-#define SECTORS_PER_PAGE (PGSIZE / BLOCK_SECTOR_SIZE)
-
+struct block *swap_block;
+struct bitmap *swap_map;
 struct lock swap_lock;
 
-struct block *swap_block;
-
-struct bitmap *swap_map;
-
+//Prit change these names and comment the old names
 void swap_init (void);
 size_t swap_out (void *frame);
 void swap_in (size_t used_index, void* frame);
